7b0676f59350a3a9f129efd35280eb5579f8471b
Sami Tuomainen <satuomainen@gmail.com>
2018-03-16 21:59:36 +0200

    Fix backend database connection

--- db/index.js
+++ db/index.js
@@ -1,34 +1,42 @@
 const debug = require('debug')('sql')
-const chalk = require('chalk')
 const Sequelize = require('sequelize')
-const pkg = require('../package.json')
 
-const name = process.env.DATABASE_NAME || pkg.name;
+const dbName = process.env.DATABASE_NAME || 'rollcall';
+const dbUsername = process.env.DATABASE_USER || 'rollcall';
+const dbPassword = process.env.DATABASE_PASSWORD || 'rollcall';
 
-const url = process.env.DATABASE_URL || `postgres://localhost:5432/${name}`;
-
-console.log(chalk.yellow(`Opening database connection to ${url}${name}`));
+console.log(`Opening database connection to ${dbName}`);
 
 // create the database instance
-const db = module.exports = new Sequelize(url, {
+const db = new Sequelize(dbName, dbUsername, dbPassword, {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
   logging: debug, // export DEBUG=sql in the environment to get SQL queries
   define: {
-    underscored: true,       // use snake_case rather than camelCase column names
-    freezeTableName: true,   // don't change table names from the one specified
-    timestamps: true,        // automatically include timestamp columns
+    underscored: true, // use snake_case rather than camelCase column names
+    freezeTableName: true, // don't change table names from the one specified
+    timestamps: true, // automatically include timestamp columns
   }
-})
+});
 
 // pull in our models
 require('./models')
 
 // sync the db, creating it if necessary
-function sync(retries=0, maxRetries=5) {
-  return db.sync({force:false})
-    .then(ok => console.log(`Synced models to db ${url}`))
+function sync(retries = 0, maxRetries = 5) {
+  return db.sync({ force: false })
+    .then(() => console.log(`Synced models to db ${dbName}`))
     .catch(fail => {
-      console.log(fail)
-    })
+      console.log(fail);
+    });
 }
 
-db.didSync = sync()
+db.didSync = sync();
+
+module.exports = db;
