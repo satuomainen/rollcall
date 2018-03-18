4766a51766ac4ba508ebc73f57ccc2273c79da00
Sami Tuomainen <satuomainen@gmail.com>
2018-03-18 13:20:55 +0200

    Remove example models that came with the generated boilerplate

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,4 @@
 'use strict';
 
-const Product = require('./product')
-const Review = require('./review');
 
-Product.hasMany(Review);
-Review.belongsTo(Product);
 
-module.exports = {Product, Review};
